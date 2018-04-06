8bc3c06226c0353dc75ddd1e62d1e2f6c082648a
Colin Loïc <loic-col@hotmail.fr>
2018-04-06 09:38:02 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,10 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'app works!';
+  title = 'Mysis';
+  songTitle = 'Johnny Hallyday - The Best Of';
+  songPrice = '15€';
+  songImg = 'http://www.laboutiquedeslecteurs.fr/media/catalog/product/cache/5/image/800x/9df78eab33525d08d6e5fb8d27136e95/j/o/johnny-hallyday.jpg';
 }
